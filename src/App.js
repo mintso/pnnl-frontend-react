a4692334a932c39f633cdb7392651d2368bfdc7d
apan09 <pan.ann@husky.neu.edu>
2021-03-20 18:02:10 -0700

    Added menu to render views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { Component } from 'react';
+import Select from 'react-select';
 import PieChart from './PieChart'
 import CalendarTable from './CalendarTable'
 import GrafanaChannel from './GrafanaChannel'
 
+const Views = [
+  { label: "Pie Chart", value: <CalendarTable /> },
+  { label: "Stacked Bar", value: "<StackedBar />" },
+  { label: "Heat Map", value: "<HeatMap />" },
+  { label: "Grafana", value: <GrafanaChannel /> }
+];
+
+
 function App(): React.Component {
   console.log('Hello');
+
+  const [selectedView, setView] = useState("");
+
+  const handleChange = (event) => {
+      console.log(event.value);
+      setView(event.value);
+  };
   
   return (
     <div>
@@ -13,6 +30,14 @@ function App(): React.Component {
         <header className="App-header">
         <p >SEB Building</p>
         </header>
+      </div>
+      <div style={{width: '200px'}}>
+	<Select placeholder="Select View"
+		options={Views}
+		onChange={handleChange} />
+      </div>
+	<p> {selectedView} </p>
+      <div>
       </div>
       <div>
         <CalendarTable />
