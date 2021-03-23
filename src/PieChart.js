ed67d3255ecc48c6fba281d7f1d4cd269004e237
luna <szymanski.l@husky.neu.edu>
2021-03-23 12:41:49 -0700

    added basic dynamic piechart

--- src/PieChart.js
+++ src/PieChart.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import piechart from './piechart.png'
+import ReactApexChart from "react-apexcharts";
 
-
-export default function PieChart() {
-    return <img src={piechart} alt="Logo" />;
+class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+          this.state = {
+            series: [44, 55, 13, 43],
+            options: {
+              chart: {
+                width: 348,
+                type: 'pie',
+              },
+              labels: ['Summer Occurrence', 'Controlled Summer', 'Controlled Winter', 'Override Release'],
+              responsive: [{
+                breakpoint: 480,
+                options: {
+                  chart: {
+                    width: 200
+                  },
+                  legend: {
+                    position: 'bottom'
+                  }
+                }
+              }]
+            },
+          };
+        }
+        render() {
+          return (
+      <div id="chart">
+  <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={350} />
+</div>
+)};
 }
+export default PieChart;
