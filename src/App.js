f9defcb241dc6108b01c614c65a67769602aeaaf
lttymm <lin.tong@husky.neu.edu>
2021-03-24 23:59:57 -0700

    split and show two stack bar separately

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Select from 'react-select';
 import CalendarTable from './CalendarTable'
 import HeatMap from './HeatMap'
-import StackedChart from './StackedChart'
+import AirflowStackedChart from './AirflowStackedChart'
+import TemperatureStackedChart from "./TemperatureStackedChart";
 
 const Views = [
-  { label: "Pie Chart", value: <CalendarTable /> },
-  { label: "Stacked Bar", value: <StackedChart /> },
-  { label: "Heat Map", value: <HeatMap title={"Heat Map of ZoneTemperature"} labels={["Summer Occurrence", "Controlled Summer", "Controlled Winter", "Override Release"]} /> } /*temp place holder */
+    {label: "Pie Chart", value: <CalendarTable/>},
+    {label: "Stacked Bar(AirFlow)", value: <AirflowStackedChart/>},
+    {label: "Stacked Bar(Temperature)", value: <TemperatureStackedChart/>},
+    {label: "Heat Map",
+        value: <HeatMap title={"Heat Map of ZoneTemperature"}
+                        labels={["Summer Occurrence", "Controlled Summer", "Controlled Winter", "Override Release"]}/>
+    } /*temp place holder */
 ];
 
 
 function App(): React.Component {
 
-  const [selectedView, setView] = useState(<CalendarTable />);
+    const [selectedView, setView] = useState(<CalendarTable/>);
 
-  const handleChange = (event) => {
-      setView(event.value);
-  };
-  
-  return (
-    <div>
-      <div className="App">
-        <header className="App-header">
-        <p >SEB Building</p>
-        </header>
-      </div>
-      <div style={{width: '200px'}}>
-        <Select placeholder="Select View"
-          options={Views}
-	  defaultValue = {{ label: "Pie Chart", value: <CalendarTable /> }}
-          onChange={handleChange} />
-      </div>
-      <div>
-	      <p> {selectedView} </p>
-      </div>
+    const handleChange = (event) => {
+        setView(event.value);
+    };
+
+    return (
         <div>
-            <iframe
-                src="https://ec2-3-140-252-185.us-east-2.compute.amazonaws.com:3000/d/pSc0RlYMzd/pnnl-seb-influx?orgId=1"
-                width="100%" height="700px"></iframe>
+            <div className="App">
+                <header className="App-header">
+                    <p>SEB Building</p>
+                </header>
+            </div>
+            <div style={{width: '200px'}}>
+                <Select placeholder="Select View"
+                        options={Views}
+                        defaultValue={{label: "Pie Chart", value: <CalendarTable/>}}
+                        onChange={handleChange}/>
+            </div>
+            <div>
+                <p> {selectedView} </p>
+            </div>
+            <div>
+                <iframe
+                    src="https://ec2-3-140-252-185.us-east-2.compute.amazonaws.com:3000/d/pSc0RlYMzd/pnnl-seb-influx?orgId=1"
+                    width="100%" height="700px"></iframe>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/TemperatureStackedChart.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
+import DataAdaptor from "./DataAdaptor";
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                stacked: true,
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+        xAxes: [
+            {
+                stacked: true,
+            },
+        ],
+    },
+}
+
+
+export default function TemperatureStackedChart() {
+    const currentDate = new Date();
+    const pastDate = new Date(currentDate.getTime() - (6 * 24 * 60 * 60 * 1000));
+
+    const tempDataObject = DataAdaptor('temperature', pastDate, currentDate);
+    const tempDateArray = [];
+    const summerOccurrenceArray = [];
+    const controlledWinterArray = [];
+    const controlledSummerArray = [];
+    const overrideReleaseArray = [];
+    for (const key in tempDataObject) {
+        tempDateArray.push(key);
+        for(const key2 in tempDataObject[key]) {
+            if(key2 === 'Summer Occurrence') {
+                summerOccurrenceArray.push(tempDataObject[key][key2]);
+            } else if(key2 === 'Controlled Winter') {
+                controlledWinterArray.push(tempDataObject[key][key2]);
+            } else if(key2 === 'Controlled Summer') {
+                controlledSummerArray.push(tempDataObject[key][key2]);
+            } else if(key2 === 'Override Release') {
+                overrideReleaseArray.push(tempDataObject[key][key2]);
+            }
+        }
+    }
+
+    const temperatureData = {
+        labels: tempDateArray,
+        datasets: [
+            {
+                label: 'Summer Occurrence',
+                data: summerOccurrenceArray,
+                backgroundColor: 'rgb(246, 193, 66)',
+            },
+            {
+                label: 'Controlled Winter',
+                data: controlledWinterArray,
+                backgroundColor: 'rgb(77, 115, 190)',
+            },
+            {
+                label: 'Controlled Summer',
+                data: controlledSummerArray,
+                backgroundColor: 'rgb(223, 130, 68)',
+            },
+            {
+                label: 'Override Release',
+                data: overrideReleaseArray,
+                backgroundColor: 'rgb(165, 165, 165)',
+            },
+        ],
+    }
+
+
+    return (
+        <>
+            <div style={{display: 'flex', justifyContent: 'start', maxWidth: 800}}>
+                <div className='header'>
+                    <h3 className='title'>Zone Temperature</h3>
+                </div>
+                <Bar data={temperatureData} options={options}/>
+            </div>
+        </>
+    );
+};
+
+
