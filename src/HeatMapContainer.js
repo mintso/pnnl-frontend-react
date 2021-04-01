21870f9e26fd62db729d94c87d1d7177679fc3ce
Leah Mamaril <lmamaril@gmail.com>
2021-04-01 10:03:55 -0700

    switch to HeatMap2

--- src/HeatMapContainer.js
+++ src/HeatMapContainer.js
@@ -1,12 +1,12 @@
-import HeatMap from './HeatMap'
+import HeatMap2 from './HeatMap2'
 
 export default function HeatMapContainer() {
     return (
         <div>
             <p>Temperature</p>
-            <HeatMap title={"Heat Map of Zone Temperature"} labels={["Controlled Summer", "Controlled Winter", "Override Release", "Summer Occurrence"]} type="temperature" />
+            <HeatMap2 title={"Heat Map of Zone Temperature"} labels={["Controlled Summer", "Controlled Winter", "Override Release", "Summer Occurrence"]} type="temperature" />
             <p>Airflow</p>
-            <HeatMap title={"Heat Map of Zone Air Flow"} labels={["Constant", "Hunting", "Normal Operation", "Override"]} type="airflow" />
+            <HeatMap2 title={"Heat Map of Zone Air Flow"} labels={["Constant", "Hunting", "Normal Operation", "Override"]} type="airflow" />
         </div>
     )
 }
