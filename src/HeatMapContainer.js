7681f7da350941b9d6b0b0cb462c9c8ceebce2fe
Leah Mamaril <lmamaril@gmail.com>
2021-04-04 18:55:30 -0700

    title removes heatmap of to be consistent with stacked bar

--- src/HeatMapContainer.js
+++ src/HeatMapContainer.js
@@ -4,9 +4,9 @@ export default function HeatMapContainer() {
     return (
         <div>
             <p>Temperature</p>
-            <HeatMap title={"Heat Map of Zone Temperature"} labels={["Controlled Summer", "Controlled Winter", "Override Release", "Summer Occurrence"]} type="temperature" />
+            <HeatMap title={"Zone Temperature"} labels={["Controlled Summer", "Controlled Winter", "Override Release", "Summer Occurrence"]} type="temperature" />
             <p>Airflow</p>
-            <HeatMap title={"Heat Map of Zone Air Flow"} labels={["Constant", "Hunting", "Normal Operation", "Override"]} type="airflow" />
+            <HeatMap title={"AirFlow"} labels={["Constant", "Hunting", "Normal Operation", "Override"]} type="airflow" />
         </div>
     )
 }
