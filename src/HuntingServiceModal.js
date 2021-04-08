188ed8e73216fad26c7cd984d9528b7227c27a99
lttymm <lin.tong@husky.neu.edu>
2021-04-07 22:52:27 -0700

    removed unecessary code

--- src/HuntingServiceModal.js
+++ src/HuntingServiceModal.js
@@ -1,35 +1,6 @@
 import './App.css';
 import React from 'react';
 import {DataGrid, GridRowsProp, GridColDef} from '@material-ui/data-grid';
-import {GridCellParams} from "@material-ui/data-grid";
-import {Button, makeStyles, Modal} from "@material-ui/core";
-
-
-function rand() {
-    return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-    const top = 50 + rand();
-    const left = 50 + rand();
-
-    return {
-        top: `${top}%`,
-        left: `${left}%`,
-        transform: `translate(-${top}%, -${left}%)`,
-    };
-}
-
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        position: 'absolute',
-        width: 550,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
 
 
 export default function HuntingServiceModal(): React.Component {
