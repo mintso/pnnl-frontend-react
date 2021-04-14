905d08f81cdc5178c2c42c9ca2fdfa68191865fa
Leah Mamaril <lmamaril@gmail.com>
2021-04-13 20:41:29 -0700

    dynamic labels incorporated

--- src/HeatMap.js
+++ src/HeatMap.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react';
 import DataAdaptor from './DataAdaptor';
 import ReactApexChart from "react-apexcharts";
 import { Date, Promise } from "globalthis/implementation";
-import GetHeatMapMonth from './GetHeatMapWeek';
 import moment from 'moment';
 import GetStartDate from './GetStartDate';
 import GetHeatMapWeek from './GetHeatMapWeek';
+import GetPredictionLabel from './GetPredictionLabel';
 
 export default function HeatMap(props) {
 
     const END_DATE = moment().subtract(1, 'days').toDate();
     let START_DATE = new Date();
     let LABELS_IDX = {};
+    const COLORS = ['#FFCF9C', '#A4D4B4', '#3B1C32', '#CA054D', '#173F5F', '#20639B', '#3CAEA3', '#F6D55C', '#ED553B', '#89045D']
+    const NUM_WEEKS = 4;
 
     // Beginning state for data and options for heatmap 
     const [state, setState] = useState({series: [
@@ -46,39 +48,7 @@ export default function HeatMap(props) {
                     radius: 0,
                     useFillColorAsStroke: true,
                     colorScale: {
-                        // TODO: this needs to be dynamic
-                        ranges: [
-                            {
-                                from: -1,
-                                to: -1,
-                                name: 'Unavailable',
-                                color: '#DEDEDE'
-                            },
-                            {
-                                from: 0,
-                                to: 0,
-                                name: props.labels[0],
-                                color: '#FFCF9C'
-                            },
-                            {
-                                from: 1,
-                                to: 1,
-                                name: props.labels[1],
-                                color: '#A4D4B4'
-                            },
-                            {
-                                from: 2,
-                                to: 2,
-                                name: props.labels[2],
-                                color: '#3B1C32'
-                            },
-                            {
-                                from: 3,
-                                to: 3,
-                                name: props.labels[3],
-                                color: '#CA054D'
-                            }
-                        ]
+                        ranges: []
                     }
                 }
             },
@@ -93,22 +63,29 @@ export default function HeatMap(props) {
 
     // changes to heatmap start here
     useEffect (() => {
-        createLabelIdx(); // map labels to index
+        
         getData();
     }, []);
   
-   // Get data by getting Sunday 4 weeks ago, retrieve data, store in series format, update state
+   // Get data by getting Sunday 4 weeks ago, retrieve data, get and set labels,
+   // store data in series format, update state
     const getData = () => {
         const p = new Promise((resolve, reject)=>{
             resolve(START_DATE.setTime(GetStartDate()));
         })
         .then(() => { return DataAdaptor(props.type, START_DATE, END_DATE); })
         .then((res) => {
+            const newLabels = GetPredictionLabel(res);
+            createLabelIdx(newLabels); // map labels to index
+            return res
+        }).then((res) =>{
+            addColors(setColorRanges);
+            return res
+        }).then((res) => { 
             // creates 4 weeks of data based on identifying the label with largest frequency
             // data not available is returned as a string '-1'
             return getHeatMapMonth(res, LABELS_IDX);
-        })
-        .then((newSeries) => {
+        }).then((newSeries) => {
             setState((prevState) => {
                 return { ...prevState, series: newSeries }
             })
@@ -117,7 +94,7 @@ export default function HeatMap(props) {
 
     // Get 4 weeks of data
     const getHeatMapMonth = (data, LABELS_IDX) => {
-        const NUM_WEEKS = 4;
+        
         // represents data for one week
         let series = [] 
 
@@ -136,15 +113,49 @@ export default function HeatMap(props) {
     return series.reverse(); 
     }
 
-
-
     // map prediction labels to a number
-    const createLabelIdx = () => {
+    const createLabelIdx = (labels) => {
         // A label with "-1" means that there is no data for that day
         LABELS_IDX['-1'] = -1;
-        for (let i=0; i<props.labels.length; i++) {
-            LABELS_IDX[props.labels[i]] = i;
+        for (let i=0; i<labels.length; i++) {
+            LABELS_IDX[labels[i]] = i;
         }
+    }
+
+    const setColorRanges =  (range) => {
+
+        setState((prevState) => {
+        return { ...prevState, options: {...prevState.options.plotOptions, plotOptions: {...prevState.options.plotOptions.heatmap, heatmap: {...prevState.options.plotOptions.heatmap.colorScale, colorScale:{...prevState.options.plotOptions.heatmap.colorScale.ranges, ranges: range}}}}  }
+        })
+    }
+
+    const addColors = (callback) => {
+
+        let range = [];
+        let i = 0;
+        let newName = '';
+        let newColor = '';    
+
+        for (const [key, value] of Object.entries(LABELS_IDX)) {
+            if (value == -1) {
+                newName = 'Unavailable'
+                newColor = '#DEDEDE';
+            } else {
+                newName = key;
+                newColor = COLORS[value%10]; // if # of labels > 10, colors are reused
+            }
+            range.push(
+                {
+                    from: value,
+                    to: value,
+                    name: newName,
+                    color: newColor
+                }
+            );
+            i++;
+          }
+
+          callback(range);
     }
 
     return (
